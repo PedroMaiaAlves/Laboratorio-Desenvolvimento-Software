f38f7b27aa2701ec1178e2b6d9e64be0f91799fb
Paulo Assis <passis@sga.pucminas.br>
2025-08-15 15:00:00 -0300

    classes de usuario, professor e aluno

--- Lab1/Aluno.java
+++ Lab1/Aluno.java
@@ -1,10 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Aluno extends Usuario{
 
     private static final int MAX_OBRIGATORIAS = 4;
     private static final int MAX_OPTATIVAS = 2;
+    private List<Disciplina> disciplinasObrigatorias;
+    private List<Disciplina> disciplinasOptativas;
 
     public Aluno(String nome, String codigoPessoa, String cpf, String senha){
         super(nome, codigoPessoa, cpf, senha);
+        this.disciplinasObrigatorias = new ArrayList<Disciplina>();
+        this.disciplinasOptativas = new ArrayList<Disciplina>();
+    }
+
+    public void addDisciplina(Disciplina disciplina) throws Exception{
+        if(disciplina.getTipoDisciplina().equals(DisciplinaType.OBRIGATORIA)){
+            if(disciplinasObrigatorias.size() < MAX_OBRIGATORIAS){
+                disciplinasObrigatorias.add(disciplina);
+            }else{
+                throw new Exception("Limite de obrigatórias atingido.");
+            }
+        }
+        else if(disciplina.getTipoDisciplina().equals(DisciplinaType.OPTATIVA)){
+            if(disciplinasOptativas.size() < MAX_OPTATIVAS){
+                disciplinasOptativas.add(disciplina);
+            }else{
+                throw new Exception("Limite de optativas atingido.");
+            }
+        }else {
+            throw new Exception("Tipo de disciplina não encontrado.");
+        }
     }
 
 }
---
+++ Lab1/Professor.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Professor extends Usuario{
+
+    private List<Disciplina> disciplinasLecionadas;
+
+    public Professor(String nome, String codiogoPessoa, String cpf, String senha) {
+        super(nome, codiogoPessoa, cpf, senha);
+        this.disciplinasLecionadas = new ArrayList<Disciplina>();
+    }
+
+    public void addDisciplina(Disciplina disciplina){
+        this.disciplinasLecionadas.add(disciplina);
+    }
+
+    public List<Disciplina> getDisciplinasLecionadas() {
+        return disciplinasLecionadas;
+    }
+
+    public void setDisciplinasLecionadas(List<Disciplina> disciplinasLecionadas) {
+        this.disciplinasLecionadas = disciplinasLecionadas;
+    }
+
+    public String getAlunosDisciplina(Disciplina disciplina){
+        String retorno = "";
+
+        if (disciplina == null || disciplina.getAlunos() == null) {
+            return "Nenhum aluno encontrado.";
+        }
+
+        for (Aluno aluno : disciplina.getAlunos()) {
+            retorno += "Nome: " + aluno.getNome() + "\n" +
+                    "Codigo Pessoa: " + aluno.getCodigoPessoa() + "\n\n";
+        }
+
+        return retorno;
+    }
+}
