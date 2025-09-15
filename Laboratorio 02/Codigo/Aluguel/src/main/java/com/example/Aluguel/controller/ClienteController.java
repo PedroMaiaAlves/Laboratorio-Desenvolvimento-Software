33b515acb1218433101eaaf544eea44d61d3d15c
Paulo Assis <passis@sga.pucminas.br>
2025-09-15 11:10:25 -0300

    atualizando nome dos endpoints

--- Laboratorio 02/Codigo/Aluguel/src/main/java/com/example/Aluguel/controller/ClienteController.java
+++ Laboratorio 02/Codigo/Aluguel/src/main/java/com/example/Aluguel/controller/ClienteController.java
@@ -9,19 +9,19 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/clientes")
+@RequestMapping("/cliente")
 @RequiredArgsConstructor
 public class ClienteController {
 
     private final ClienteService clienteService;
 
-    @PostMapping
+    @PostMapping("/cadastrar")
     public ResponseEntity<ClienteDTO> createCliente(@RequestBody ClienteDTO clienteDTO) {
         ClienteDTO createdCliente = clienteService.createCliente(clienteDTO);
         return ResponseEntity.ok(createdCliente);
     }
 
-    @GetMapping
+    @GetMapping("/listar")
     public ResponseEntity<List<ClienteDTO>> getAllClientes() {
         List<ClienteDTO> clientes = clienteService.getAllClientes();
         return ResponseEntity.ok(clientes);
@@ -33,7 +33,7 @@ public class ClienteController {
         return ResponseEntity.ok(cliente);
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("/atualizar/{id}")
     public ResponseEntity<ClienteDTO> updateCliente(@PathVariable Long id, @RequestBody ClienteDTO clienteDTO) {
         ClienteDTO updatedCliente = clienteService.updateCliente(id, clienteDTO);
         return ResponseEntity.ok(updatedCliente);
